9ae2fe82cf6f1159f783dc6d747729dd6d29543c
Nassime <nassimeelkamari2002@gmail.com>
2024-04-13 00:48:29 +0000

    components added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import Navbar from "@/components/navbar/Navbar";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import "@/app/global.css"
+import Footer from "@/components/footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,8 +19,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+      <div className="container">
         <Navbar/>
         {children}
+        <Footer/>
+        </div>
         </body>
     </html>
   );
--- src/components/navbar/links/navLink/navLink.tsx
+++ src/components/navbar/links/navLink/navLink.tsx
@@ -1,17 +1,11 @@
 "use client"
 import Link from 'next/link'
 import styles from './navLink.module.css'
-import { usePathname } from 'next/navigation'
 
 
 
-const NavLink = ({item}) => {
-
-    const pathName = usePathname();
-
-    return (
-        <Link href={item.path} className={`${styles.container} ${pathName === item.path && styles.active}`} >{item.title}</Link>
-    )
-}
+const NavLink = ({item}:any) => (
+    <Link href={item.path} className={styles.container}>{item.title}</Link>
+)
 
 export default NavLink
